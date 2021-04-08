8bcfa6a82c746b7552d6588a76c9229fc24e7449
elson <elsonchen1999@gmail.com>
2021-04-07 20:10:38 -0700

    added base functions to script.js

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+function dark_theme(){
+    //when button click
+        //background color of side bar, textarea, and button change to darker colors
+        //text on dark theme change to light theme
+}
+
+function light_theme(){
+    //when button click
+        ////background color of side bar, textarea, and button change back to normal
+        //text on dark theme change to dark theme
+}
+
+function cancel(){
+    //hide text area, save, and cancel buttons
+
+}
+
+function New_Note(){
+    //reveal text area, save, and cancel buttons
+
+}
+
+function save(){
+    //alert user they are about to save note
+    //prompt for the title
+    //add JS object to notesArray with title and text in textarea as body
+    //add list item to sidebar with title of object
+}
+
+function list_search(){
+    //search through notesArray and find object usig title, then display text in tex area
+
+}
+
+let note1 = {
+    title: "note one",
+    body: "this is my first note",
+};
+
+let note2 = {
+    title: "note 2",
+    body: "Note 2 begins here",
+};
+
+let notesArray = [note1,note2]
+
+
